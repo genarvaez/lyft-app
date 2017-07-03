d16ecf6e086a623fc53c9c222ce590416e9085ac
genarvaez <ge_naravez@hotmail.com>
2017-07-03 12:39:46 -0400

    terminado seccion de index para signup de usuario

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,31 @@
 
  /*Side bar*/
 
- $(".button-collapse").sideNav();
 
- 
-  $(document).ready(function() {
-    $('select').material_select();
-  });
-       
+  $(".button-collapse").sideNav();
+  $(".alert").hide();
+/*INDEX SIGN UP*/ 
+  var nombre = $(".input1-index3");
+  var email = $(".input2-index3");
+  var boton = $(".btn-index3");
+  var enlace = $(".enlace");
+  
+  boton.click(function(){
+
+    if(nombre.val() == "" || !(/[a-zA-Z]/).test(nombre.val())){
+      $(".alert").show(400);
+      enlace.preventDefault();
+    }
+    else if(!(/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/.test(email.val()))){
+      $(".alert").show(400);
+      enlace.preventDefault();
+    }
+    else if(!$(".filled-in").prop('checked')){
+        $(".alert").show(400);
+        enlace.preventDefault();
+       }
+    else{
+      enlace.attr('href', 'index4.html');
+      $(".alert").hide()
+    }
+  })
